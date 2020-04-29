39e9ec6fdc237d9860f2c2a67fd8b6d9bbec3147
batch <baaaakkrad@gmail.com>
2020-04-30 01:06:46 +0900

    add friendlychat project

---
+++ friendlychat/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return FriendlychatApp();
+  }
+}
+
+class FriendlychatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Friendlychat",
+      home: ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Friendlychat"),
+      ),
+    );
+  }
+}
