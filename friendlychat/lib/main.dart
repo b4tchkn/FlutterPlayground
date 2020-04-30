a782a2b99dc0d0f6966397540bba2e50c9bf639f
batch <baaaakkrad@gmail.com>
2020-04-30 16:11:32 +0900

    Implement a message list

--- friendlychat/lib/main.dart
+++ friendlychat/lib/main.dart
@@ -73,3 +73,35 @@ class ChatScreenState extends State<ChatScreen> {
   }
 
 }
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text});
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    final String _name = "Your Name";
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(child: Text(_name[0]),),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(_name, style: Theme.of(context).textTheme.subhead,),
+              Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
