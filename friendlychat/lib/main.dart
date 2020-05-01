f023d515fe564de61d7011aee101977f388a4ba9
batch <baaaakkrad@gmail.com>
2020-05-01 17:37:18 +0900

    add Expanded for ChatMessage

--- friendlychat/lib/main.dart
+++ friendlychat/lib/main.dart
@@ -142,18 +142,20 @@ class ChatMessage extends StatelessWidget {
                 child: Text(_name[0]),
               ),
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  _name,
-                  style: Theme.of(context).textTheme.subhead,
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 5.0),
-                  child: Text(text),
-                )
-              ],
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    _name,
+                    style: Theme.of(context).textTheme.subhead,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 5.0),
+                    child: Text(text),
+                  )
+                ],
+              ),
             )
           ],
         ),
