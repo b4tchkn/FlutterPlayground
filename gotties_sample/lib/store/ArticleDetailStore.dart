9a78748d1844e6da46c3667ce2dd2039baf92d26
batch <baaaakkrad@gmail.com>
2020-05-03 18:58:22 +0900

    prototype image fade animation

--- gotties_sample/lib/store/ArticleDetailStore.dart
+++ gotties_sample/lib/store/ArticleDetailStore.dart
@@ -1,9 +1,25 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class ArticleDetailStore with ChangeNotifier {
+  CrossFadeState crossFadeState = CrossFadeState.showFirst;
+  Timer crossFadeTimer;
+
   String dateFormat(DateTime dateTime) {
     DateFormat formatter = DateFormat('yyyy-MM-dd');
     return formatter.format(dateTime);
+  }
+
+  startCrossFadeImage() {
+    crossFadeTimer = Timer.periodic(Duration(seconds: 6),
+        if (crossFadeState == CrossFadeState.showFirst) {
+      crossFadeState = CrossFadeState.showSecond;
+    } else {
+    crossFadeState = CrossFadeState.showSecond;
+    }
+    notifyListeners();
+    );
   }
 }
--- gotties_sample/lib/widgets/MovieContainer.dart
+++ gotties_sample/lib/widgets/MovieContainer.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:gottiessample/store/ArticleDetailStore.dart';
+import 'package:provider/provider.dart';
 
 class MovieContainer extends StatelessWidget {
   final beforeImageURL;
@@ -10,23 +14,44 @@ class MovieContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      // https://flutter.dev/docs/cookbook/images/fading-in-images
-      // https://pub.dev/packages/cached_network_image#-readme-tab-
-      child: CachedNetworkImage(
-        placeholder: (context, beforeImageURL) => Center(
-          child: const CircularProgressIndicator(
-            backgroundColor: Colors.red,
+    final beforeImage = Image.network(
+      beforeImageURL,
+      height: 300.0,
+    );
+    final afterImage = Image.network(afterImageURL, height: 300.0);
+    return Column(children: <Widget>[
+      Center(
+        // https://flutter.dev/docs/cookbook/images/fading-in-images
+        // https://pub.dev/packages/cached_network_image#-readme-tab-
+        child: CachedNetworkImage(
+          placeholder: (context, beforeImageURL) => Center(
+            child: const CircularProgressIndicator(
+              backgroundColor: Colors.red,
+            ),
+          ),
+          imageUrl: afterImageURL,
+          height: 300,
+          fadeOutDuration: const Duration(seconds: 1),
+          fadeInDuration: const Duration(seconds: 3),
+          errorWidget: (context, beforeImageURL, error) => Center(
+            child: const Icon(Icons.error),
           ),
         ),
-        imageUrl: afterImageURL,
-        height: 300,
-        fadeOutDuration: const Duration(seconds: 1),
-        fadeInDuration: const Duration(seconds: 3),
-        errorWidget: (context, beforeImageURL, error) => Center(
-          child: const Icon(Icons.error),
+      ),
+      Center(
+        child: AnimatedCrossFade(
+          firstChild: beforeImage,
+          secondChild: afterImage,
+          duration: Duration(seconds: 5),
+          crossFadeState:
+              Provider.of<ArticleDetailStore>(context).crossFadeState,
         ),
       ),
-    );
+      RaisedButton(
+        child: Text('Start Fade'),
+        onPressed:
+            Provider.of<ArticleDetailStore>(context).startCrossFadeImage(),
+      )
+    ]);
   }
 }
