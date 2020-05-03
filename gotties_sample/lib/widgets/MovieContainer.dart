0c36c26c6a1cb1d869c6d231ceb5d6a87492518e
batch <baaaakkrad@gmail.com>
2020-05-03 16:42:07 +0900

    replace FareInImage to CachedNetworkImage

--- gotties_sample/lib/widgets/MovieContainer.dart
+++ gotties_sample/lib/widgets/MovieContainer.dart
@@ -1,6 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:transparent_image/transparent_image.dart';
 
 class MovieContainer extends StatelessWidget {
   final beforeImageURL;
@@ -10,22 +10,18 @@ class MovieContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     // what's loadingBuilder
     // https://api.flutter.dev/flutter/widgets/Image/loadingBuilder.html
-    return Image.network(beforeImageURL, height: 400.0, loadingBuilder:
-        (BuildContext context, Widget child, ImageChunkEvent loadingProgress) {
-      if (loadingProgress == null) return child;
-      return Stack(
-        children: <Widget>[
-          Center(child: CircularProgressIndicator()),
-          Center(
-            // https://flutter.dev/docs/cookbook/images/fading-in-images
-            child: FadeInImage.memoryNetwork(
-              placeholder: kTransparentImage,
-              image: beforeImageURL,
-              fadeInCurve: Curves.easeOut,
-            ),
+    return Center(
+      // https://flutter.dev/docs/cookbook/images/fading-in-images
+      child: CachedNetworkImage(
+        placeholder: (context, beforeImageURL) => Center(
+          child: const CircularProgressIndicator(
+            backgroundColor: Colors.red,
           ),
-        ],
-      );
-    });
+        ),
+        imageUrl: beforeImageURL,
+        fadeOutDuration: const Duration(seconds: 1),
+        fadeInDuration: const Duration(seconds: 3),
+      ),
+    );
   }
 }
