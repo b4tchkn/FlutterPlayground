c99fe79e4c1bc5c8cfa8780ce85b5f168b6fb2ef
batch <baaaakkrad@gmail.com>
2020-05-04 14:29:21 +0900

    fix image with using Stack and Opacity

--- gotties_sample/lib/widgets/MovieContainer.dart
+++ gotties_sample/lib/widgets/MovieContainer.dart
@@ -1,8 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:gottiessample/store/ArticleDetailStore.dart';
-import 'package:provider/provider.dart';
 
 class MovieContainer extends StatelessWidget {
   final beforeImageURL;
@@ -18,44 +16,52 @@ class MovieContainer extends StatelessWidget {
     );
     final afterImage = Image.network(afterImageURL, height: 300.0);
     return Column(children: <Widget>[
+      Center(
+        // https://flutter.dev/docs/cookbook/images/fading-in-images
+        // https://pub.dev/packages/cached_network_image#-readme-tab-
+        child: Stack(
+          children: <Widget>[
+            CachedNetworkImage(
+              imageUrl: beforeImageURL,
+              height: 300,
+            ),
+            Opacity(
+              // 0~1: 0だとこいつが完全に透明になる．1だと現れる
+              opacity: 0,
+              child: CachedNetworkImage(
+                placeholder: (context, beforeImageURL) => Center(
+                  child: const CircularProgressIndicator(
+                    backgroundColor: Colors.red,
+                    valueColor: AlwaysStoppedAnimation<Color>(Colors.red),
+                  ),
+                ),
+                imageUrl: afterImageURL,
+                height: 300,
+                fadeOutDuration: const Duration(seconds: 1),
+                fadeInDuration: const Duration(seconds: 1),
+                errorWidget: (context, beforeImageURL, error) => Center(
+                  child: const Icon(Icons.error),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
 //      Center(
-//        // https://flutter.dev/docs/cookbook/images/fading-in-images
-//        // https://pub.dev/packages/cached_network_image#-readme-tab-
-//        child: CachedNetworkImage(
-//          placeholder: (context, beforeImageURL) => Center(
-//            child: const CircularProgressIndicator(
-//              backgroundColor: Colors.red,
-//            ),
-//          ),
-//          imageUrl: afterImageURL,
-//          height: 300,
-//          fadeOutDuration: const Duration(seconds: 1),
-//          fadeInDuration: const Duration(seconds: 3),
-//          errorWidget: (context, beforeImageURL, error) => Center(
-//            child: const Icon(Icons.error),
+//        child: GestureDetector(
+//          onTap: () {
+//            Provider.of<ArticleDetailStore>(context).startCrossFadeImage();
+//          },
+//          child: AnimatedCrossFade(
+//            firstChild: beforeImage,
+//            secondChild: afterImage,
+//            duration: Duration(seconds: 5),
+//            crossFadeState:
+//                Provider.of<ArticleDetailStore>(context).crossFadeState,
 //          ),
 //        ),
 //      ),
-      Center(
-        child: GestureDetector(
-          onTap: () {
-            Provider.of<ArticleDetailStore>(context).startCrossFadeImage();
-          },
-          child: AnimatedCrossFade(
-            firstChild: beforeImage,
-            secondChild: afterImage,
-            duration: Duration(seconds: 5),
-            crossFadeState:
-                Provider.of<ArticleDetailStore>(context).crossFadeState,
-          ),
-        ),
-      ),
-      Text(Provider.of<ArticleDetailStore>(context).isPlaying.toString()),
-//      RaisedButton(
-//        child: Text('Start Fade'),
-//        onPressed:
-//            Provider.of<ArticleDetailStore>(context).startCrossFadeImage(),
-//      )
+//      Text(Provider.of<ArticleDetailStore>(context).isPlaying.toString()),
     ]);
   }
 }
