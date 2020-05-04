d0f7e5e11c0fadd6b9cfa89bcecca250419b3a0f
batch <baaaakkrad@gmail.com>
2020-05-04 12:38:54 +0900

    can play TKUMI

--- gotties_sample/lib/store/ArticleDetailStore.dart
+++ gotties_sample/lib/store/ArticleDetailStore.dart
@@ -1,11 +1,14 @@
 import 'dart:async';
 
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class ArticleDetailStore with ChangeNotifier {
   CrossFadeState crossFadeState = CrossFadeState.showFirst;
-  Timer crossFadeTimer;
+  bool isPlaying = false;
+  Timer _crossFadeTimer;
 
   String dateFormat(DateTime dateTime) {
     DateFormat formatter = DateFormat('yyyy-MM-dd');
@@ -13,7 +16,20 @@ class ArticleDetailStore with ChangeNotifier {
   }
 
   startCrossFadeImage() {
-    crossFadeTimer = Timer.periodic(Duration(seconds: 6), (timer) {
+    // https://medium.com/@pongpiraupra/a-comprehensive-guide-to-playing-local-mp3-files-with-seek-functionality-in-flutter-7730a453bb1a
+    AudioPlayer advancedPlayer = AudioPlayer();
+    AudioCache audioCache = AudioCache(fixedPlayer: advancedPlayer);
+
+    if (isPlaying) {
+      advancedPlayer.stop();
+      isPlaying = false;
+    } else {
+      audioCache.play('takumi.mp3');
+      isPlaying = true;
+    }
+    notifyListeners();
+
+    _crossFadeTimer = Timer.periodic(Duration(seconds: 3), (timer) {
       if (crossFadeState == CrossFadeState.showFirst) {
         crossFadeState = CrossFadeState.showSecond;
       } else {
--- gotties_sample/lib/widgets/MovieContainer.dart
+++ gotties_sample/lib/widgets/MovieContainer.dart
@@ -18,38 +18,44 @@ class MovieContainer extends StatelessWidget {
     );
     final afterImage = Image.network(afterImageURL, height: 300.0);
     return Column(children: <Widget>[
+//      Center(
+//        // https://flutter.dev/docs/cookbook/images/fading-in-images
+//        // https://pub.dev/packages/cached_network_image#-readme-tab-
+//        child: CachedNetworkImage(
+//          placeholder: (context, beforeImageURL) => Center(
+//            child: const CircularProgressIndicator(
+//              backgroundColor: Colors.red,
+//            ),
+//          ),
+//          imageUrl: afterImageURL,
+//          height: 300,
+//          fadeOutDuration: const Duration(seconds: 1),
+//          fadeInDuration: const Duration(seconds: 3),
+//          errorWidget: (context, beforeImageURL, error) => Center(
+//            child: const Icon(Icons.error),
+//          ),
+//        ),
+//      ),
       Center(
-        // https://flutter.dev/docs/cookbook/images/fading-in-images
-        // https://pub.dev/packages/cached_network_image#-readme-tab-
-        child: CachedNetworkImage(
-          placeholder: (context, beforeImageURL) => Center(
-            child: const CircularProgressIndicator(
-              backgroundColor: Colors.red,
-            ),
-          ),
-          imageUrl: afterImageURL,
-          height: 300,
-          fadeOutDuration: const Duration(seconds: 1),
-          fadeInDuration: const Duration(seconds: 3),
-          errorWidget: (context, beforeImageURL, error) => Center(
-            child: const Icon(Icons.error),
+        child: GestureDetector(
+          onTap: () {
+            Provider.of<ArticleDetailStore>(context).startCrossFadeImage();
+          },
+          child: AnimatedCrossFade(
+            firstChild: beforeImage,
+            secondChild: afterImage,
+            duration: Duration(seconds: 5),
+            crossFadeState:
+                Provider.of<ArticleDetailStore>(context).crossFadeState,
           ),
         ),
       ),
-      Center(
-        child: AnimatedCrossFade(
-          firstChild: beforeImage,
-          secondChild: afterImage,
-          duration: Duration(seconds: 5),
-          crossFadeState:
-              Provider.of<ArticleDetailStore>(context).crossFadeState,
-        ),
-      ),
-      RaisedButton(
-        child: Text('Start Fade'),
-        onPressed:
-            Provider.of<ArticleDetailStore>(context).startCrossFadeImage(),
-      )
+      Text(Provider.of<ArticleDetailStore>(context).isPlaying.toString()),
+//      RaisedButton(
+//        child: Text('Start Fade'),
+//        onPressed:
+//            Provider.of<ArticleDetailStore>(context).startCrossFadeImage(),
+//      )
     ]);
   }
 }
