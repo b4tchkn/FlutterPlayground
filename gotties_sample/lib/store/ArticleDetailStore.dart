770919d0b94aa2462ecf3b7176c207a076131965
batch <baaaakkrad@gmail.com>
2020-05-04 14:46:56 +0900

    add GestureDetector for MovieContainer

--- gotties_sample/lib/store/ArticleDetailStore.dart
+++ gotties_sample/lib/store/ArticleDetailStore.dart
@@ -1,14 +1,10 @@
-import 'dart:async';
-
 import 'package:audioplayers/audio_cache.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class ArticleDetailStore with ChangeNotifier {
-  CrossFadeState crossFadeState = CrossFadeState.showFirst;
   bool isPlaying = false;
-  Timer _crossFadeTimer;
 
   AudioPlayer _advancedPlayer = AudioPlayer();
   AudioCache _audioCache;
@@ -18,7 +14,7 @@ class ArticleDetailStore with ChangeNotifier {
     return formatter.format(dateTime);
   }
 
-  startCrossFadeImage() {
+  startBeforeAfter() {
     // https://medium.com/@pongpiraupra/a-comprehensive-guide-to-playing-local-mp3-files-with-seek-functionality-in-flutter-7730a453bb1a
 //    _advancedPlayer = AudioPlayer();
     _audioCache = AudioCache(fixedPlayer: _advancedPlayer);
@@ -33,14 +29,5 @@ class ArticleDetailStore with ChangeNotifier {
       isPlaying = true;
     }
     notifyListeners();
-
-    _crossFadeTimer = Timer.periodic(Duration(seconds: 3), (timer) {
-      if (crossFadeState == CrossFadeState.showFirst) {
-        crossFadeState = CrossFadeState.showSecond;
-      } else {
-        crossFadeState = CrossFadeState.showSecond;
-      }
-      notifyListeners();
-    });
   }
 }
--- gotties_sample/lib/widgets/MovieContainer.dart
+++ gotties_sample/lib/widgets/MovieContainer.dart
@@ -1,6 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:gottiessample/store/ArticleDetailStore.dart';
+import 'package:provider/provider.dart';
 
 class MovieContainer extends StatelessWidget {
   final beforeImageURL;
@@ -16,36 +18,41 @@ class MovieContainer extends StatelessWidget {
     );
     final afterImage = Image.network(afterImageURL, height: 300.0);
     return Column(children: <Widget>[
-      Center(
-        // https://flutter.dev/docs/cookbook/images/fading-in-images
-        // https://pub.dev/packages/cached_network_image#-readme-tab-
-        child: Stack(
-          children: <Widget>[
-            CachedNetworkImage(
-              imageUrl: beforeImageURL,
-              height: 300,
-            ),
-            Opacity(
-              // 0~1: 0だとこいつが完全に透明になる．1だと現れる
-              // https://bukiyo-papa.com/opacity/
-              opacity: 0,
-              child: CachedNetworkImage(
-                placeholder: (context, beforeImageURL) => Center(
-                  child: const CircularProgressIndicator(
-                    backgroundColor: Colors.red,
-                    valueColor: AlwaysStoppedAnimation<Color>(Colors.red),
+      GestureDetector(
+        onTap: () {
+          Provider.of<ArticleDetailStore>(context).startBeforeAfter();
+        },
+        child: Center(
+          // https://flutter.dev/docs/cookbook/images/fading-in-images
+          // https://pub.dev/packages/cached_network_image#-readme-tab-
+          child: Stack(
+            children: <Widget>[
+              CachedNetworkImage(
+                imageUrl: beforeImageURL,
+                height: 300,
+              ),
+              Opacity(
+                // 0~1: 0だとこいつが完全に透明になる．1だと現れる
+                // https://bukiyo-papa.com/opacity/
+                opacity: 0,
+                child: CachedNetworkImage(
+                  placeholder: (context, beforeImageURL) => Center(
+                    child: const CircularProgressIndicator(
+                      backgroundColor: Colors.red,
+                      valueColor: AlwaysStoppedAnimation<Color>(Colors.red),
+                    ),
+                  ),
+                  imageUrl: afterImageURL,
+                  height: 300,
+                  fadeOutDuration: const Duration(seconds: 1),
+                  fadeInDuration: const Duration(seconds: 1),
+                  errorWidget: (context, beforeImageURL, error) => Center(
+                    child: const Icon(Icons.error),
                   ),
                 ),
-                imageUrl: afterImageURL,
-                height: 300,
-                fadeOutDuration: const Duration(seconds: 1),
-                fadeInDuration: const Duration(seconds: 1),
-                errorWidget: (context, beforeImageURL, error) => Center(
-                  child: const Icon(Icons.error),
-                ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
 //      Center(
