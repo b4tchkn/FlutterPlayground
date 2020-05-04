b4291347fefa0616c4ba4ebf18f04c3ca3382b6e
batch <baaaakkrad@gmail.com>
2020-05-04 12:50:32 +0900

    add music stop feature when tap Image widget

--- gotties_sample/lib/store/ArticleDetailStore.dart
+++ gotties_sample/lib/store/ArticleDetailStore.dart
@@ -10,6 +10,9 @@ class ArticleDetailStore with ChangeNotifier {
   bool isPlaying = false;
   Timer _crossFadeTimer;
 
+  AudioPlayer _advancedPlayer = AudioPlayer();
+  AudioCache _audioCache;
+
   String dateFormat(DateTime dateTime) {
     DateFormat formatter = DateFormat('yyyy-MM-dd');
     return formatter.format(dateTime);
@@ -17,14 +20,16 @@ class ArticleDetailStore with ChangeNotifier {
 
   startCrossFadeImage() {
     // https://medium.com/@pongpiraupra/a-comprehensive-guide-to-playing-local-mp3-files-with-seek-functionality-in-flutter-7730a453bb1a
-    AudioPlayer advancedPlayer = AudioPlayer();
-    AudioCache audioCache = AudioCache(fixedPlayer: advancedPlayer);
+//    _advancedPlayer = AudioPlayer();
+    _audioCache = AudioCache(fixedPlayer: _advancedPlayer);
 
     if (isPlaying) {
-      advancedPlayer.stop();
+      print("STOP");
+      _advancedPlayer.stop();
       isPlaying = false;
     } else {
-      audioCache.play('takumi.mp3');
+      print("START");
+      _audioCache.play('takumi.mp3');
       isPlaying = true;
     }
     notifyListeners();
